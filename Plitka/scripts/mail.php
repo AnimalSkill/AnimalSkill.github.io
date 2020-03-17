ba6ca532df8a975b89fe1c1e3a631d552d13b6a3
AnimalSkill <31277139+AnimalSkill@users.noreply.github.com>
2020-03-17 11:53:37 +0300

    POST to GET

--- Plitka/scripts/mail.php
+++ Plitka/scripts/mail.php
@@ -3,10 +3,9 @@
 $recepient = "max.boyka@mail.ru";
 $sitename = "Свой плиточник";
 
-$name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
 $message = "Имя: $name \nТелефон: $phone;
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
-?>
